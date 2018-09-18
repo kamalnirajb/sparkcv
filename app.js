303aa5578b79d335cdb8ca2acf2dfc861571f614
Niraj Kumar <kamal.nirajb@gmail.com>
2018-09-19 01:06:51 +0530

    Form creation

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var i18n = require("i18n-express");
+var ejs = require('ejs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,12 +11,19 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
-app.engine('html', require('ejs').renderFile);
+app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(i18n({
+  translationsPath: path.join(__dirname, 'i18n'), // <--- use here. Specify translations files path.
+  siteLangs: ["en", "hi"],
+  textsVarName: 'translation'
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 var router = express.Router();
+var locations = require(__dirname + '/assets/locations/cities.json');
+var sca = require(__dirname + '/assets/single_choice_attributes.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    profile: {
+      sca: sca,
+      locations: locations
+    }
+  });
 });
 
 module.exports = router;
